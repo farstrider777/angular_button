14a0ccb59272606a851577521d61cfaddbc75a5f
Joshua Bradford <jjyet@yahoo.com>
2017-03-13 14:44:56 -0400

    added test text field where it writes to the page immediately, added like button with s that dissappears at the right time.

--- src/js/controllers/counter.js
+++ src/js/controllers/counter.js
@@ -1,11 +1,25 @@
 //import angular from 'angular';
+var count = 0;
 
 function LikeCounter ($scope) {
   console.log('test');
 
+  $scope.count = count;
+  $scope.s = 's';
+
   $scope.addLike = function () {
     console.log('like');
+    count++;
+    console.log(count);
+    $scope.count = count;
+    if(count === 1){
+      $scope.s = '';
+    }else{
+      $scope.s = 's';
+    }
   };
 }
+
+LikeCounter.$inject = ['$scope'];
 
 export default LikeCounter;
